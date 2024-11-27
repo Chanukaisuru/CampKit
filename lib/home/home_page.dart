81caa2316d3f9e9280303731607a7c97d2fadf94
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-27 12:32:48 +0530

    create search icon

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -25,10 +25,10 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
-          child: const Column(
+          child: Column(
             children: [
               // First Row: Greeting and Profile Image
-              Row(
+              const Row(
                 children: [
                   // Column for greeting text
                   Expanded(
@@ -56,14 +56,45 @@ class HomePage extends StatelessWidget {
                   // Profile image
                   CircleAvatar(
                     radius: 25,
-                    backgroundImage: AssetImage('assets/user_profile.jpg'), // Replace with your image asset
+                    backgroundImage: AssetImage('assets/image/userimage.jpeg'), // Replace with your image asset
                   ),
                 ],
-              ),
-          ],
+              ),  
+
+              /*--------Search bar----------*/
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color:Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0,3),
+                    ),
+                  ]
+                ),
+
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search hear",
+                    prefixIcon: Icon(Icons.search,color: Colors.black,),
+                    filled: true,
+                    fillColor: Colors.transparent,
+                    contentPadding: EdgeInsets.symmetric(vertical:16),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ), 
+            ],
+          ),
         ),
       ),
-    ),
     );
     
   }         
