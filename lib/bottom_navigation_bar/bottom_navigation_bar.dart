2b27d481e10626f02a3854104fb69cfd70537fc6
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-28 11:25:24 +0530

    new file:   lib/bottom_navigation_bar/bottom_navigation_bar.dart     create code for bottom navigation bar

---
+++ lib/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNavBar({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      items: [
+        _buildNavBarItem(
+          iconPath: 'assets/icon/home.png',
+          label: 'Home',
+          isSelected: currentIndex == 0,
+        ),
+        _buildNavBarItem(
+          iconPath: 'assets/icon/search.png',
+          label: 'Search',
+          isSelected: currentIndex == 1,
+        ),
+        _buildNavBarItem(
+          iconPath: 'assets/icon/cart.png',
+          label: 'Cart',
+          isSelected: currentIndex == 2,
+        ),
+        _buildNavBarItem(
+          iconPath: 'assets/icon/user.png',
+          label: 'Profile',
+          isSelected: currentIndex == 3,
+        ),
+      ],
+      backgroundColor: const Color.fromARGB(221, 255, 255, 255),
+      selectedItemColor: Colors.white,
+      unselectedItemColor: Colors.grey,
+      type: BottomNavigationBarType.fixed,
+      elevation: 0,
+    );
+  }
+
+  BottomNavigationBarItem _buildNavBarItem({
+    required String iconPath,
+    required String label,
+    required bool isSelected,
+  }) {
+    return BottomNavigationBarItem(
+      icon: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          color: isSelected ? Colors.green : Colors.transparent,
+          shape: BoxShape.circle,
+        ),
+        child: Image.asset(
+          iconPath,
+          height: 24,
+          width: 24,
+          color: isSelected ? Colors.white : Colors.grey,
+        ),
+      ),
+      label: label,
+    );
+  }
+}
