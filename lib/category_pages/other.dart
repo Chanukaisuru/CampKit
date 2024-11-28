625e81de53dcebddd7b08b6d3188a1e42130d474
Chanukaisuru <chanukaisuru26@gmail.com>
2024-11-28 12:49:03 +0530

    create other page and put in to bottom navigation bar

--- lib/category_pages/other.dart
+++ lib/category_pages/other.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:campkit/bottom_navigation_bar/bottom_navigation_bar.dart';
 
-class OtherPage extends StatelessWidget {
+class OtherPage extends StatefulWidget {
   const OtherPage({super.key});
 
   @override
+  _OtherPageState createState() => _OtherPageState();
+}
+
+class _OtherPageState extends State<OtherPage> {
+  int _selectedIndex = 0;
+
+  void _onNavBarItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: const [
+              Text('Tent page'),
+            ],
+          ),
+        ),
+      ),
+
+      /*-----------------bottom navigation bar------------------ */
+      bottomNavigationBar: CustomBottomNavBar(
+        currentIndex: _selectedIndex,
+        onTap: _onNavBarItemTapped,
+      ),
+    );
   }
 }
