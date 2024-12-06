951cf59c334dc0a0d93467ea40e1b0ec7b20a0b0
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-06 12:37:12 +0530

    create tent card category_pages

---
+++ lib/category_pages/tent/tent_card.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class TentCard extends StatelessWidget {
+  final String imageUrl;
+  final String capacity;
+  final int pricePerDay;
+  final VoidCallback onAddToCart;
+
+  const TentCard({
+    required this.imageUrl,
+    required this.capacity,
+    required this.pricePerDay,
+    required this.onAddToCart,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+      elevation: 4,
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                imageUrl,
+                height: 100,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              capacity,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+
+            const SizedBox(height: 4),
+            const Text(
+              'with rain cover',
+              style: TextStyle(
+                fontSize: 12,
+                color: Colors.grey,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+            Text(
+              'Rs. $pricePerDay/- per day',
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: onAddToCart,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF277A8C),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              
+              child: const Text(
+                'Add to Cart',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
