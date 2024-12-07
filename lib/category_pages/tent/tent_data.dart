97cfe4f8595a66c7467944e9df361aef5d40a27e
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-07 16:05:28 +0530

    add name

--- lib/category_pages/tent/tent_data.dart
+++ lib/category_pages/tent/tent_data.dart
@@ -1,18 +1,20 @@
 class Tent {
   final String imageUrl;
+  final String name;
   final String capacity;
   final int price;
 
   Tent({
     required this.imageUrl,
+    required this.name,
     required this.capacity,
     required this.price,
   });
 }
 
 final List<Tent> tents = [
-  Tent(imageUrl: 'assets/image/4person.jpg', capacity: '4 person tent', price: 500),
-  Tent(imageUrl: 'assets/image/6person.jpeg', capacity: '6 person tent', price: 800),
-  Tent(imageUrl: 'assets/image/8person.jpeg', capacity: '8 person tent', price: 1100),
-  Tent(imageUrl: 'assets/image/10person.jpeg', capacity: '10 person tent', price: 1600),
+  Tent(imageUrl: 'assets/image/4person.jpg', name: 'manual tent', capacity: '4 person tent', price: 500),
+  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'manual tent', capacity: '6 person tent', price: 800),
+  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'manual tent', capacity: '8 person tent', price: 1100),
+  Tent(imageUrl: 'assets/image/10person.jpeg', name: 'manual tent', capacity: '10 person tent', price: 1600),
 ];
