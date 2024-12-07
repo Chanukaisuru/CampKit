dbecfbbadab5bec1db02c454d2f3f3b2375c8e26
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-07 19:22:44 +0530

    change capacity and add discription tent_data. dart

--- lib/category_pages/tent/tent_data.dart
+++ lib/category_pages/tent/tent_data.dart
@@ -1,21 +1,25 @@
 class Tent {
   final String imageUrl;
   final String name;
-  final String capacity;
+  final String? capacity;
   final int price;
+  final String? description;
 
   Tent({
     required this.imageUrl,
     required this.name,
-    required this.capacity,
+    this.capacity,
     required this.price,
+    this.description,
+
   });
 }
 
 final List<Tent> tents = [
-  Tent(imageUrl: 'assets/image/4person.jpg', name: 'Manual Tent', capacity: '4 Person ', price: 500),
-  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'Auto Tent', capacity: '6 Person', price: 800),
-  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'Manual Tent', capacity: '8 Person ', price: 1100),
+  Tent(imageUrl: 'assets/image/4person.jpg', name: 'Manual Tent', capacity: '4 Person ', price: 500, description: 'with rain cover'),
+  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'Auto Tent', capacity: '6 Person', price: 800,description: 'with rain cover'),
+  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'Manual Tent', capacity: '8 Person ', price: 1100,description: 'with rain cover'),
   Tent(imageUrl: 'assets/image/10person.jpeg', name: 'Manual Tent', capacity: '10 Person ', price: 1600),
+  Tent(imageUrl: 'assets/image/bell_tent.jpg', name: 'Manual Tent', price: 4000),
 ];
 
