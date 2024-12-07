7e34e236eafa8173a5ed298a06da7d6bfb77c6c7
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-07 20:27:19 +0530

    modified:   lib/category_pages/tent/tent_card.dart

--- lib/category_pages/tent/tent_card.dart
+++ lib/category_pages/tent/tent_card.dart
@@ -1,25 +1,109 @@
-class Tent {
+import 'package:flutter/material.dart';
+
+class TentCard extends StatelessWidget {
   final String imageUrl;
   final String name;
   final String? capacity;
-  final int price;
+  final int pricePerDay;
   final String? description;
+  final VoidCallback onAddToCart;
 
-  Tent({
+  const TentCard({
     required this.imageUrl,
     required this.name,
     this.capacity,
-    required this.price,
+    required this.pricePerDay,
     this.description,
-
+    required this.onAddToCart,
   });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
+       
+      elevation: 4,
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            /*------------card image-------------------- */
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.asset(
+                imageUrl,
+                height: 100,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+            ),
+            
+            /*-----------------name---------------------- */
+            const SizedBox(height: 8),
+            Text(
+              name,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+
+            /*----------------capacity--------------------------- */
+            const SizedBox(height: 8),
+            if(capacity !=null)
+            Text(
+              capacity!,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 14,
+              ),
+            ),
+            
+            /**-------------description------------------- */
+            const SizedBox(height: 8),
+            if(description !=null)
+            
+            Text(
+              description!,
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.grey,
+              ),
+            ),
+
+            /*---------------------price--------------------*/
+            const SizedBox(height: 8),
+            Text(
+              'Rs. $pricePerDay/- per day',
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            
+            /*------------------------cart button----------------------- */
+            const SizedBox(height: 8),
+            ElevatedButton(
+              onPressed: onAddToCart,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color(0xFF277A8C),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              
+              
+              child: const Text(
+                'Add to Cart',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
-
-final List<Tent> tents = [
-  Tent(imageUrl: 'assets/image/4person.jpg', name: 'Manual Tent', capacity: '4 Person ', price: 500, description: 'with rain cover'),
-  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'Auto Tent', capacity: '6 Person', price: 800, description: 'with rain cover'),
-  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'Manual Tent', capacity: '8 Person ', price: 1100, description: 'with rain cover'),
-  Tent(imageUrl: 'assets/image/10person.jpeg', name: 'Manual Tent', capacity: '10 Person ', price: 1600),
-  Tent(imageUrl: 'assets/image/bell_tent.jpg', name: 'Manual Tent', price: 4000),
-];
-
