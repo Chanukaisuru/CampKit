0357ffdff28e1b137bc4c7719f948c1a4c841d82
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-07 20:13:14 +0530

    modified:   lib/category_pages/tent/tent_data.dart

--- lib/category_pages/tent/tent_data.dart
+++ lib/category_pages/tent/tent_data.dart
@@ -17,8 +17,8 @@ class Tent {
 
 final List<Tent> tents = [
   Tent(imageUrl: 'assets/image/4person.jpg', name: 'Manual Tent', capacity: '4 Person ', price: 500, description: 'with rain cover'),
-  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'Auto Tent', capacity: '6 Person', price: 800,description: 'with rain cover'),
-  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'Manual Tent', capacity: '8 Person ', price: 1100,description: 'with rain cover'),
+  Tent(imageUrl: 'assets/image/6person.jpeg', name: 'Auto Tent', capacity: '6 Person', price: 800, description: 'with rain cover'),
+  Tent(imageUrl: 'assets/image/8person.jpeg', name: 'Manual Tent', capacity: '8 Person ', price: 1100, description: 'with rain cover'),
   Tent(imageUrl: 'assets/image/10person.jpeg', name: 'Manual Tent', capacity: '10 Person ', price: 1600),
   Tent(imageUrl: 'assets/image/bell_tent.jpg', name: 'Manual Tent', price: 4000),
 ];
