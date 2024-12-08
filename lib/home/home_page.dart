25ff5b83e8c287d27b3ad85c1cc8ace103ca5e44
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-08 13:37:42 +0530

    delete search bar in homepage

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -131,46 +131,7 @@ class _HomePageState extends State<HomePage> {
               /*-----------------Search bar-------------------*/
 
               SizedBox(height: 20),
-              Container(
-                decoration: BoxDecoration(
-                  color:Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                      offset: Offset(0,3),
-                    ),
-                  ]
-                ),
-
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: "Search hear",
-                    hintStyle: TextStyle(color: Colors.grey),
-                    
-                    prefixIcon: Padding(
-                      padding: const EdgeInsets.all(12.0),
-                      child: Image.asset('assets/icon/search.png',
-                      width: 20,
-                      height: 20,
-                      color: const Color.fromARGB(255, 121, 120, 120),
-                      
-                      fit: BoxFit.contain,
-                      ),
-                    ),
-
-                    filled: true,
-                    fillColor: Colors.transparent,
-                    contentPadding: EdgeInsets.symmetric(vertical:16),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                      borderSide: BorderSide.none,
-                    ),
-                  ),
-                ),
-              ), 
+              
 
 
 
