e667dd17f3ac764549bc1d69c9bb2b8f06640aec
K G K Fernando <kalharageethaka12@gmail.com>
2024-12-16 13:13:32 +0530

    Created Cooking item data with images
    Created cooking_item_card.dart with assests.

---
+++ lib/category_pages/cooking item/cooking_item_data.dart
@@ -0,0 +1,21 @@
+class CookingItem {
+  final String imageUrl;
+  final String name;
+  final String description;
+  final int price;
+
+  CookingItem({
+    required this.imageUrl,
+    required this.name,
+    required this.description,
+    required this.price,
+  });
+}
+
+List<CookingItem> cookingItems = [
+  CookingItem(imageUrl: 'assets/images/item1.jpg', name: 'Cooking Pot', description: 'A large pot for cooking', price: 1200, ),
+  CookingItem(imageUrl: 'assets/images/item2.jpg', name: 'Grill Set', description: 'Grill for outdoor cooking', price: 1500, ),
+  CookingItem(imageUrl: 'assets/images/item3.jpg', name: 'Knife Set', description: 'High-quality stainless steel knives.', price: 1500, ),
+  CookingItem(imageUrl: 'assets/images/item4.jpg', name: 'Cutting Board', description: 'Wooden cutting board for food prep.', price: 800, ),
+  CookingItem(imageUrl: 'assets/images/item5.jpg', name: 'Cast Iron Skillet', description: 'Heavy-duty skillet for frying.', price: 2000, ),
+];
