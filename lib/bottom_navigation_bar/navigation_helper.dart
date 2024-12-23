d66b41beb6730a13206d9e48c4222f67d92c0933
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-23 16:12:25 +0530

    create navigation_helper.dart

---
+++ lib/bottom_navigation_bar/navigation_helper.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:campkit/home/home_page.dart';
+import 'package:campkit/category_pages/tent/tent.dart';
+import 'package:campkit/cart/my_cart_page.dart';
+
+class NavigationHelper {
+  static void onNavBarItemTapped(
+    BuildContext context,
+    int index,
+    int currentIndex,
+  ) {
+    if (index == currentIndex) return; // Prevent navigation to the same page.
+
+    switch (index) {
+      case 0:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+        break;
+      case 1:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const TentPage(initialCategory: "Tent")),
+        );
+        break;
+      case 2:
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MyCartPage(initialCategory: "Cart")),
+        );
+        break;
+      case 3:
+        // Implement the Profile page navigation logic
+        break;
+    }
+  }
+}
