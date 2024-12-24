77d10d1db9297ef0503e34c004dac62281b90092
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-24 13:06:43 +0530

    add remove icon for card

--- lib/cart/cart_page.dart
+++ lib/cart/cart_page.dart
@@ -14,6 +14,27 @@ class MyCartPage extends StatefulWidget {
 class _MyCartPageState extends State<MyCartPage> {
   int _selectedIndex = 2;
 
+  // List of cart items
+  List<Map<String, dynamic>> cartItems = [
+    {
+      'image': 'assets/image/4person.jpg',
+      'title': '4 Person Tent',
+      'subtitle': 'with rain cover\nRs.500/- per day',
+      'price': 500,
+    },
+    {
+      'image': 'assets/image/4person.jpg',
+      'title': 'Gas Canister',
+      'subtitle': 'Can cook 2 hours',
+      'price': 1000,
+    },
+  ];
+
+  void removeCartItem(int index) {
+    setState(() {
+      cartItems.removeAt(index);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,26 +85,26 @@ class _MyCartPageState extends State<MyCartPage> {
                       ),
                     ],
                   ),
-                  child: ListView(
-                    children: [
-                      CartItem(
-                        image: 'assets/image/4person.jpg',
-                        title: '4 Person Tent',
-                        subtitle: 'with rain cover\nRs.500/- per day',
-                        price: 500,
-                      ),
-                      SizedBox(height: 16),
-                      CartItem(
-                        image: 'assets/image/4person.jpg',
-                        title: 'Gas Canister',
-                        subtitle: 'Can cook 2 hours',
-                        price: 1000,
-                      ),
-                    ],
+                  child: ListView.builder(
+                    itemCount: cartItems.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          CartItem(
+                            image: cartItems[index]['image'],
+                            title: cartItems[index]['title'],
+                            subtitle: cartItems[index]['subtitle'],
+                            price: cartItems[index]['price'],
+                            onRemove: () => removeCartItem(index),
+                          ),
+                          if (index != cartItems.length - 1)
+                            SizedBox(height: 16),
+                        ],
+                      );
+                    },
                   ),
                 ),
               ),
-
 
               SizedBox(height: 16),
 
@@ -138,7 +159,6 @@ class _MyCartPageState extends State<MyCartPage> {
       bottomNavigationBar: CustomBottomNavBar(
         currentIndex: _selectedIndex,
         onTap: (index) => NavigationHelper.onNavBarItemTapped(context, index, _selectedIndex),
-
       ),
     );
   }
@@ -150,114 +170,131 @@ class CartItem extends StatelessWidget {
   final String title;
   final String subtitle;
   final int price;
+  final VoidCallback onRemove;
 
   const CartItem({
     required this.image,
     required this.title,
     required this.subtitle,
     required this.price,
+    required this.onRemove,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.shade200,
-            blurRadius: 5,
+    return Stack(
+      children: [
+        Container(
+          padding: EdgeInsets.all(12),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.shade200,
+                blurRadius: 5,
+              ),
+            ],
           ),
-        ],
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Checkbox
-          Checkbox(value: true, onChanged: (value) {}),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Image of the item
+              Image.asset(
+                image,
+                width: 130,
+                height: 130,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(width: 20),
 
-          // Image of the item
-          Image.asset(
-            image,
-            width: 130,
-            height: 130,
-            fit: BoxFit.cover,
-          ),
-
-          SizedBox(width: 20),
-
-          // Item description (title, subtitle, and actions)
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                // Title
-                Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(height: 4),
-
-                // Subtitle
-                Text(
-                  subtitle,
-                  style: TextStyle(
-                    fontSize: 14,
-                  ),
-                ),
-                SizedBox(height: 8),
-
-                // Price
-                Text(
-                  'Rs.$price/-',
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.teal,
-                  ),
-                ),
-                SizedBox(height: 12),
-
-                // Add/Remove Icons and Quantity
-                Row(
+              // Item description (title, subtitle, and actions)
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    GestureDetector(
-                      onTap: () {},
-                      child: Image.asset(
-                        'assets/icon/remove.png',
-                        width: 35,
-                        height: 35,
-                      ),
-                    ),
-                    SizedBox(width: 10),
+                    // Title
                     Text(
-                      '1',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    SizedBox(width: 10),
-                    GestureDetector(
-                      onTap: () {},
-                      child: Image.asset(
-                        'assets/icon/add.png',
-                        width: 32,
-                        height: 32,
+                      title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
                       ),
+                    ),
+                    SizedBox(height: 4),
+
+                    // Subtitle
+                    Text(
+                      subtitle,
+                      style: TextStyle(
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 8),
+
+                    // Price
+                    Text(
+                      'Rs.$price/-',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal,
+                      ),
+                    ),
+                    SizedBox(height: 12),
+
+                    // Add/Remove Icons and Quantity
+                    Row(
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset(
+                            'assets/icon/remove.png',
+                            width: 35,
+                            height: 35,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          '1',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        SizedBox(width: 10),
+                        GestureDetector(
+                          onTap: () {},
+                          child: Image.asset(
+                            'assets/icon/add.png',
+                            width: 32,
+                            height: 32,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+
+        // Close button (cross mark)
+        Positioned(
+          top: 8,
+          right: 8,
+          child: GestureDetector(
+            onTap: onRemove,
+            child: Image.asset(
+                            'assets/icon/add.png',
+                            width: 32,
+                            height: 32,
+                          ),
+          ),
+        ),
+      ],
     );
   }
 }
+
 
 /* --------------Summary Row Widget--------------- */
 class SummaryRow extends StatelessWidget {
