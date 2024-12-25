d0b26c2d7623c57bdff875d4c92b0228a255bef1
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-25 12:59:31 +0530

    add camera icon for change the profile pic

--- lib/category_pages/profile/components/body.dart
+++ lib/category_pages/profile/components/body.dart
@@ -14,6 +14,27 @@ class Body extends StatelessWidget {
               CircleAvatar(
                 backgroundImage: AssetImage("assets/image/userimage.jpeg"),
               ),
+
+              Positioned(
+                right: 0,
+                bottom: 0,
+                child: SizedBox(
+                  height: 46,
+                  width: 46,
+                  child: TextButton(
+                    onPressed:() {
+                      //logic hear
+                    }, 
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.grey.shade200,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                    ),
+                    child: Image.asset('assets/icon/camera.png'),
+                  ),
+                ),
+              ),  
             ],  
             ),
         ),
