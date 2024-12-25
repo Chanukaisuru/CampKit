c2206591495c60fdc738bd20281950b499a28a2a
Chanukaisuru <chanukaisuru26@gmail.com>
2024-12-25 12:24:00 +0530

    create body dart page for profile screan  page

---
+++ lib/category_pages/profile/components/body.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children:[
+        SizedBox(
+          height: 115,
+          width: 115,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage("assets/image/profile.jpg"),
+              ),
+            ],  
+            ),
+        ),
+      ], 
+    );
+    
+  }
+}
