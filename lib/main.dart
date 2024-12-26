fbb35801e1f8a9f03fa485887c6e0248b43baf7b
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2024-12-26 23:59:41 +0530

    modified:   lib/main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
-import 'package:campkit/onboarding_page.dart';
+import 'package:firebase_core/firebase_core.dart'; // Import Firebase core package
 import 'package:flutter/material.dart';
+import 'package:campkit/onboarding_page.dart';
 
-
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // Ensure binding for async initialization
+  await Firebase.initializeApp(); // Initialize Firebase
   runApp(const MyApp()); 
 }
 
@@ -15,12 +17,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-      
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  OnboardingPage(),
+      home: OnboardingPage(),
     );
   }
 }
-  
