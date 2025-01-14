c3595a71c8eea1d0fcce65342300104c784cf442
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-01-14 18:51:01 +0530

    implement login status check in OnboardingPage

--- lib/onboarding_page.dart
+++ lib/onboarding_page.dart
@@ -1,73 +1,88 @@
 import 'package:flutter/material.dart';
-import 'signIn_and_signUp/sign_in_page.dart';
-
+import 'package:shared_preferences/shared_preferences.dart';
+import '../home/home_page.dart';
+import '../signIn_and_signUp/sign_in_page.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
 
+  Future<void> _checkLoginStatus(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+    if (isLoggedIn) {
+      // Navigate to HomePage if session exists
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => HomePage()),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    _checkLoginStatus(context);
+
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SafeArea(       
+      body: SafeArea(
         child: Center(
           child: Column(
             children: [
               SizedBox(height: 30),
-                Image.asset(
-                  'assets/image/OnbordingPage_image.jpeg',
-                  width: 400,
-                  height: 400,
-                ),
-              
+              Image.asset(
+                'assets/image/OnbordingPage_image.jpeg',
+                width: 400,
+                height: 400,
+              ),
               SizedBox(height: 30),
-              Text("CampKit",
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 36,
-                color: Colors.orange,
+              Text(
+                "CampKit",
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 36,
+                  color: Colors.orange,
+                ),
               ),
-              ),
-
               SizedBox(height: 20),
-              Padding(padding: const EdgeInsets.symmetric(horizontal: 30.0),
-              child: Text('Rent top-quality level gear for \nyour adventures. \nExplore, book, and get \nready to go!',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 18,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                child: Text(
+                  'Rent top-quality level gear for \nyour adventures. \nExplore, book, and get \nready to go!',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
               ),
-              ),
-              ),
-
               SizedBox(height: 25),
-
               ElevatedButton(
                 onPressed: () {
-                  Navigator.push(context,
-                   MaterialPageRoute(builder: (context) =>SignInPage(),
-                   ),
-                   );
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SignInPage(),
+                    ),
+                  );
                 },
-               style: ElevatedButton.styleFrom(
+                style: ElevatedButton.styleFrom(
                   backgroundColor: Color(0xFF277A8C),
                   padding: EdgeInsets.symmetric(horizontal: 100, vertical: 15),
                   shape: RoundedRectangleBorder(
-                     borderRadius: BorderRadius.circular(30),
+                    borderRadius: BorderRadius.circular(30),
                   ),
                 ),
                 child: Text(
                   'Get Start',
-                   style: TextStyle(
-                   fontSize: 20,
-                   color: Colors.white,
-                   ),
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
                 ),
               ),
-
             ],
           ),
         ),
-        
       ),
     );
   }
